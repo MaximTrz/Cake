7a977356bdf73ca9176f4d2e1c515d03d096130c
Maxim <maxim.trz@gmail.com>
2019-02-26 13:54:16 +0500

    26/02/2019 2

--- js/sctipt.js
+++ js/sctipt.js
@@ -1,3 +1,5 @@
+
+
 window.addEventListener('DOMContentLoaded', ()=>{
     const menuButton = document.querySelectorAll('.buttonMenuBox'),
           menuItems = document.querySelector('.menuItems'),
@@ -5,21 +7,31 @@ window.addEventListener('DOMContentLoaded', ()=>{
           logoBox = document.querySelector('.logoBox'),
           phoneNumber = document.querySelector('.menu__phone>a'),
           menu = document.querySelector('.menu');
-
-
-
+    
+    ///////////////Восстановление меню при изменении окна
+    window.addEventListener('resize', ()=>{
+            if(window.innerWidth>768){
+                menuItems.style.display = 'flex';                
+                menu.style.display='flex';            
+                phoneNumber.style.display = 'block';                
+            } else {
+                menuItems.style.display = 'none';  
+            }
+    });
+        
+    /////////////////Кнопка меню
     menuButton.forEach(function(button){
         button.addEventListener('click', ()=>{            
             if (menuItems.style.display == 'block'){
-                menuItems.classList.remove('animated');
-                menuItems.classList.remove('bounceOutLeft');
+                menuItems.classList.remove('fadeInDown');
+                menuItems.classList.remove('shake');
                 menuItems.style.display = 'none';                
                 menu.style.display='flex';            
                 phoneNumber.style.display = 'block';
                 logoBox.classList.remove('menuSM');            
             } else {
                 menuItems.classList.add('animated');
-                menuItems.classList.add('bounceInLeft');
+                menuItems.classList.add('fadeInDown');
                 menuItems.style.display = 'block';
                 menu.style.display='block';            
                 phoneNumber.style.display = 'none';
